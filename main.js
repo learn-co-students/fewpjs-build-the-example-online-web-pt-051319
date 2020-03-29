e22656328aefa410e2a90a82c5abe9373f671206
blitzparanoia <bluemilk44@gmail.com>
2020-03-29 17:10:36 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
 
+  document.addEventListener("click", e => {
+
+    const errorDiv = document.getElementById("modal");
+    errorDiv.classList.add("hidden");
+    if (e.target.className === "like") {
+      console.log("like clicked");
+      mimicServerCall()
+        .then(() => {
+          console.log("success");
+          const heart = e.target.querySelector(".like-glyph");
+          if (heart.classList.contains("activated-heart")) {
+            heart.classList.remove("activated-heart");
+          } else {
+            heart.classList.add("activated-heart");
+          }
+        })
+        .catch(function (error) {
+          console.log("failure");
+          errorDiv.classList.remove("hidden");
+        });
+    }
+  });
+});
 
 
 
